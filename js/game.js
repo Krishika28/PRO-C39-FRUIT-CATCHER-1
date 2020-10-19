476be2dbd80ba06456b466cec76bdd4ef80fba70
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2020-10-19 16:18:21 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -109,7 +109,7 @@ class Game{
                  }
                  
                   if (player.index !== null) {
-                     (var i = 0; i < fruitGroup.length; i++) 
+                    for (var i = 0; i < fruitGroup.length; i++) 
                       { if (fruitGroup.get(i).isTouching(players)
                            ) { fruitGroup.get(i).destroy();
                               player.score =player.score+1; player.update();
